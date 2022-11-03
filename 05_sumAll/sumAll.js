59efdbf123353f2c365b20d3102cfb12fb1744e6
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2022-11-03 16:21:40 +0100

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    const array = [start, end];
 
+    console.log(array.every(element => element > 0 || element != null))
+
+    if ((start < 0 || end < 0)) {
+        return 'ERROR';
+    } else if (typeof start !== 'number' || typeof end !== 'number') { 
+        return 'ERROR';
+    } else if (start > end) {
+        array.reverse();
+    }
+
+    let sum = 0;
+    
+    for (let i = array[0]; i <= array[1]; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
