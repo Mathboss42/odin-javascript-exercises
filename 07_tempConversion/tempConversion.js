04f55da29b5dabaf6bad7381bae06383ec66537a
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2022-11-03 17:29:49 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(baseTemp) {
+  let convertedTemp = (baseTemp - 32) * (5/9);
+  let roundedTemp = Math.round(convertedTemp * 10) / 10;
+  return roundedTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(baseTemp) {
+  let convertedTemp = baseTemp * (9/5) + 32;
+  let roundedTemp = Math.round(convertedTemp * 10) / 10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
